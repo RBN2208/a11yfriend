52baaf67abb11068db98573b436e343b2f6dedc6
robinsteinhauser <robin.steinhauser@tallence.com>
2025-12-12 07:52:16 +0100

    update i18n translations, add custom verified and reset pages, some more updates

---
+++ src/shared/components/profile-settings/menus/profile-settings-account-menu.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import {useTranslations} from "next-intl";
+
+type Props = {}
+
+export default function ProfileSettingsAccountMenu(props: Props) {
+  const t = useTranslations();
+
+  return (
+    <>
+       Account Menu
+    </>
+  )
+}
